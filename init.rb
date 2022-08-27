f85c35b756e648ae660e4c5a9520cb87d39416a4
Massimo Rossello <counseling.mr@gmail.com>
2022-08-27 17:31:52 +0200

    Version 5.0.2

--- init.rb
+++ init.rb
@@ -1,12 +1,12 @@
 # encoding: utf-8
-require_dependency 'sidebar_hook_listener'
+require_relative 'lib/sidebar_hook_listener'
 
 Redmine::Plugin.register :sidebar_hide do
   name 'Sidebar Hide Plugin'
   author 'Berk Demirkır, Massimo Rossello'
   description 'This plugin provides ability to hide sidebar'
-  version '4.2.1'
+  version '5.0.2'
   url 'https://github.com/maxrossello/sidebar_hide'
   author_url 'https://github.com/maxrossello'
-  requires_redmine :version_or_higher => '4.2.0'
+  requires_redmine :version_or_higher => '5.0.2'
 end
