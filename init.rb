ccc086ca1af34c67f247cc34be4b071c72c8fdff
Michaël <michael.broutin@gmail.com>
2023-08-30 09:36:19 +0200

    Bump version number => 5.0.3

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :sidebar_hide do
   name 'Sidebar Hide Plugin'
   author 'Berk Demirkır, Massimo Rossello'
   description 'This plugin provides ability to hide sidebar'
-  version '5.0.2'
+  version '5.0.3'
   url 'https://github.com/maxrossello/sidebar_hide'
   author_url 'https://github.com/maxrossello'
   requires_redmine :version_or_higher => '5.0.2'
